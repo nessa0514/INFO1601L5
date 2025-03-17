0181f56ccec0df6de76beb531dfb9537ac84f0b3
nessa0514 <aneesa.mitchell@my.uwi.edu>
2025-03-17 13:06:43 -0400

    task 2

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
-function happyPrint(string){
-  console.log("😀: "+string);
- }
- 
- function sadPrint(string){
-  console.log("😢: "+string);
- }
- 
- //This high-order function adds 2 parameters and passes the answer to the callback
- function add(a, b, callback){
-    let ans = a + b;
-    callback(ans);// call the callback and pass the answer to it
- }
- 
- //call add passing the callbacks to it
- 
- add(5, 10, happyPrint);
- add(11, 12, sadPrint);
+ //prints current date
+ function printDate(){
+  console.log(Date().toLocaleTimeString());
+}
+     
+setInterval(printDate, 1000);
