92eeace9e6509f05d4e927cf550a50ce842660fd
nessa0514 <aneesa.mitchell@my.uwi.edu>
2025-03-17 11:25:00 -0400

    task 3

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-let radius = 7;
-const pi = 3.14;
+// let radius = 7;
+// const pi = 3.14;
 
-let area = radius * radius * pi;
+// let area = radius * radius * pi;
+
+let name = "bob";
+let age = 24;
+
+console.log(typeof(name))//string
+console.log(typeof(age))//number
+
+console.log(`Hello my name is ${name}, I'm ${age} years old`);
+//Hello my name is bob, I'm 24 years old.
+
+console.log(`I was born in ${2020 - age}`)
+//I was born in 1966
